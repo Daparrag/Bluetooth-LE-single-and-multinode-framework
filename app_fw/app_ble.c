808ca0002d9cf343c400991aededc7e8e9800a9a
Daparrag <daparrag@correo.udistrital.edu.co>
2017-06-05 11:24:11 +0200

    resolve the application firmware dependecies for init the BLE devices, and BLE profile

--- app_fw/app_ble.c
+++ app_fw/app_ble.c
@@ -2,8 +2,8 @@
 /*application management functions*/
 /*global procedures common to any application */
 
-#include "app_ble.h"
-
+#include <app_ble.h>
+#include <list.h>
 
 
 
@@ -11,10 +11,10 @@
 uint16_t service_handle, dev_name_char_handle, appearance_char_handle;
 volatile uint8_t  bnrg_expansion_board = IDB04A1;
 const uint8_t DEVICE_BDADDR[] = { 0x55, 0x11, 0x07, 0x01, 0x16, 0xE1 }; /*central device addrs*/
+
 /************************************************************/
 
-
-tBleStatus (*const  GAP_INIT_FUNC[BLE_ARCH_MASK+1])(uint8_t ,uint8_t ,uint8_t ,uint16_t* ,
+tBleStatus(*const  GAP_INIT_FUNC [BLE_ARCH_MASK+1])(uint8_t ,uint8_t ,uint8_t ,uint16_t* ,
                                                  uint16_t* ,uint16_t* ) = {                                             
 MY_HAVE_IDB0xA1(0, aci_gap_init),                                            
 MY_HAVE_IDB0xA1(1, aci_gap_init)                                                    
@@ -22,13 +22,26 @@ MY_HAVE_IDB0xA1(1, aci_gap_init)
 
 #define  aci_gap_init(_role,_privacity_enable,_device_name_char_length,_service_handler,\
                       _dev_name_char_handle,_appearance_char_handle,arch) \
-        ((*GAP_INIT_FUNC[arch&BLE_ARCH_MASK])(_role,_privacity_enable,_device_name_char_length,\
+        ((*GAP_INIT_FUNC[arch & BLE_ARCH_MASK])(_role,_privacity_enable,_device_name_char_length,\
                                               _service_handler,_dev_name_char_handle,\
                                               _appearance_char_handle))/*init gap function*/
+/**
+  * @brief  This function initialize the profile list.
+  * @ This function must be called at the begining of the application.
+  * @param profile datastructure.
+  * @
+  */
+void profile_control_init(app_profile_t * profile){
+    LIST_STRUCT_INIT(profile, _service);
+    profile->n_service=0;
+}
+/**
+  * @brief  This function is called to add Start the BLE APP firware.
+  * @param void.
+  * @retval APP_Status: Value indicating success or error code.
+  */
 
-
-
-APP_Status APP_Init_BLE(void){
+APP_Status APP_Init_BLE(void){/*can be used by any application*/
 
   uint8_t ret = 0;
   uint8_t  hwVersion;
@@ -54,7 +67,7 @@ APP_Status APP_Init_BLE(void){
 
   if (ret != BLE_STATUS_SUCCESS)
   {
-    return BV_APP_ERROR;
+    return APP_ERROR;
   }
 
    /* Set auth requirement*/
@@ -67,32 +80,65 @@ APP_Status APP_Init_BLE(void){
   /* Set output power level */
   aci_hal_set_tx_power_level(1, 5);
   
-  return BV_APP_SUCCESS;
+  return APP_SUCCESS;
 
+}
+
+/**
+  * @brief  This function is called to add any Service.
+  * @param  service_uuid: Service uuid value.
+  * @param  service_handle: Pointer to a variable in which the service handle will be saved.
+  * @retval APP_Status: Value indicating success or error code.
+  */
+
+APP_Status APP_add_BLE_Service(app_profile_t * profile, app_service_t * service){
+  tBleStatus ret;
+  LIST_STRUCT_INIT(service,_attr); /*Initialized the attribute list associate to this service*/
+  list_add(profile->_service,service); /*Add the new service at the list of services*/
+  ret = aci_gatt_add_serv(service->service_uuid_type,
+                          service->ServiceUUID,
+                          service->service_type,
+                          service->max_attr_records,
+                          &(service->ServiceHandle));
+  if (ret != BLE_STATUS_SUCCESS)
+  {
+    /*remove the service form the list list_chop*/
+    return APP_ERROR;
+  }                          
+  profile->n_service+=1; /*increment the control service counter*/
+ return APP_SUCCESS;
 }
 
 
 
+/**
+  * @brief  This function is called to add any characteristic.
+  * @param  service_uuid: Service uuid value.
+  * @param  service_handle: Pointer to a variable in which the service handle will be saved.
+  * @retval APP_Status: Value indicating success or error code.
+  */
 
+static app_service_t * get_tail_service(app_profile_t profile){
+	struct app_service_t * temp_p;
+	if(*profile==NULL){
+		return NULL;
+	}
 
-
-
-
-
-
-
-void app_Error_Handler(void){
-	
+	for(l=*profile.service;l->next_service != NULL;l->next_service);
+		return l;
 }
 
 
+static app_service_t * get_tail_characteristic(app_profile_t profile, app_service_t service){
 
-APP_Status app_AddService (sync_uuid.ServiceUUID, &handlers.ServiceHandle){
+}
+
+static app_char_t * get_tail_value(app_profile_t profile, app_service_t service, app_char_t chart){
 
 }
 
 
-APP_Status app_AddChar (){
-
+add_service(app_profile_t profile, app_service_t* service){
+	/*declare the service name*/
 
 }
